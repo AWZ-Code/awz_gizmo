62e423868a26396c74ae7cba5b7c5a7f1a295c14
AxeelWarZ <axeelwarz@gmail.com>
2026-03-26 17:01:35 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version "cerulean"
+games {"rdr3"}
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+lua54 "yes"
+
+author ''
+description 'Gizmo for RedM'
+version '1.1.6'
+
+shared_script 'config.lua'
+
+client_scripts {
+  'utils/*.lua',
+  'client/*.lua'
+}
+
+ui_page 'web/dist/index.html'
+
+files {
+  'locales/*.json',
+	'web/dist/index.html',
+	'web/dist/**/*',
+}
+
+provide 'object_gizmo'
